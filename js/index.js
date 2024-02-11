d037fa7f39fbf075f39159f6bfc9f7e2b602bfc4
Bianca <biancamathieu@yahoo.com>
2024-02-11 15:52:40 +1100

    Mega Menu and Hero image Button
    mega menu adjusted to be inline with navbar (could not work out close button to save my life) and added smooth scroll with hero image button Our Rooms

--- js/index.js
+++ js/index.js
@@ -14,3 +14,30 @@ carouselOurRooms.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+
+
+
+//nav bar dropdown
+    // const room = document.querySelector('.rooms');
+    // const menuArea = document.querySelector('.menuArea');
+    // const closeButton = document.querySelector('.closeButton');
+
+    // room.addEventListener('click', () => {
+    //     menuArea.style.display = 'block';        
+    //     }); 
+
+        
+    // closeButton.addEventListener('click', () => {
+    //     console.log('help, display none not working?');
+    //     menuArea.style.display = 'none';
+    // });
+
+ //scroll to rooms
+    
+const heroButton = document.querySelector('.hero-button');
+const ourRooms = document.querySelector('.ourRooms');
+
+heroButton.addEventListener('click', () => {
+    ourRooms.scrollIntoView({ behavior: 'smooth' });
+});
